c4bc15138e12e2a6812fde1896f6e14207add4ec
Yohai Wiener <yohaiwiener@gmail.com>
2025-04-02 09:10:52 +0100

    broke arrow key navigation ~:

--- kilo.c
+++ kilo.c
@@ -12,6 +12,14 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey
+{
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
+
 /*** data ***/
 struct termios original_termios;
 
@@ -66,7 +74,7 @@ void enableRawMode(void)
     }
 }
 
-char editorReadKey(void)
+int editorReadKey(void)
 {
     int nread;
     char c;
@@ -77,7 +85,43 @@ char editorReadKey(void)
             die("read");
         }
     }
-    return c;
+
+    if (c == '\x1b')
+    {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+        {
+            return '\x1b';
+        }
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+        {
+            return '\x1b';
+        }
+
+        if (seq[0] == '[')
+        {
+            switch (seq[1])
+            {
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                }
+            }
+        }
+        return '\x1b';
+    }
+    else
+    {
+        return c;
+    }
 }
 
 int getCursorPosition(int *rows, int *cols)
@@ -133,20 +177,21 @@ int getWindowSize(int *rows, int *cols)
 }
 
 /*** input ****/
-void editorMoveCursor(char key)
+void editorMoveCursor(int key)
 {
     switch (key)
     {
-    case 'a':
+    case ARROW_LEFT:
         E.cx--;
         break;
-    case 'd':
+    case ARROW_RIGHT:
+        break;
         E.cx++;
         break;
-    case 'w':
+    case ARROW_UP:
         E.cy--;
         break;
-    case 's':
+    case ARROW_DOWN:
         E.cy++;
         break;
     }
@@ -154,7 +199,7 @@ void editorMoveCursor(char key)
 
 void editorProcessKeypress(void)
 {
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch (c)
     {
     case CTRL_KEY('q'):
@@ -163,10 +208,10 @@ void editorProcessKeypress(void)
         exit(0);
         break;
         //-----------------------------
-    case 'a':
-    case 'd':
-    case 'w':
-    case 's':
+    case ARROW_DOWN:
+    case ARROW_UP:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
         editorMoveCursor(c);
         break;
         //-----------------------------
