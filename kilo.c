0e6d99d7c58b5cc0336db566c71ee3d19c2b6756
Yohai Wiener <yohaiwiener@gmail.com>
2025-04-04 20:32:21 +0300

    another midpoint for ch.3

--- kilo.c
+++ kilo.c
@@ -17,7 +17,11 @@ enum editorKey
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** data ***/
@@ -101,16 +105,37 @@ int editorReadKey(void)
 
         if (seq[0] == '[')
         {
-            switch (seq[1])
+            if (seq[1] >= '0' && seq[1] <= '9')
             {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                {
+                    return '\x1b';
+                }
+                if (seq[2] == "~")
+                {
+                    switch (seq[1])
+                    {
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    }
+                }
+            }
+            else
+            {
+
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                }
             }
         }
         return '\x1b';
@@ -179,16 +204,28 @@ void editorMoveCursor(int key)
     switch (key)
     {
     case ARROW_LEFT:
-        E.cx--;
+        if (E.cx != 0)
+        {
+            E.cx--;
+        }
         break;
     case ARROW_RIGHT:
-        E.cx++;
+        if (E.cx != E.screencols - 1)
+        {
+            E.cx++;
+        }
         break;
     case ARROW_UP:
-        E.cy--;
+        if (E.cy != 0)
+        {
+            E.cy--;
+        }
         break;
     case ARROW_DOWN:
-        E.cy++;
+        if (E.cy != E.screenrows - 1)
+        {
+            E.cy++;
+        }
         break;
     }
 }
@@ -203,7 +240,18 @@ void editorProcessKeypress(void)
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
-        //-----------------------------
+    //-----------------------------
+    case PAGE_DOWN:
+    case PAGE_UP:
+    {
+        int times = E.screenrows;
+        while (times--)
+        {
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+    }
+    break;
+    //-----------------------------
     case ARROW_DOWN:
     case ARROW_UP:
     case ARROW_LEFT:
